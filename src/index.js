47cfa79d5e17b10e1e24f425b0a69f84471df29d
David Thorn <david.thorn221278@googlemail.com>
2018-11-17 21:43:41 +0100

    created basic canvas showing all dots

---
+++ src/index.js
@@ -0,0 +1,35 @@
+/// Neural network
+
+
+/// Teams 
+const bvb = 0;
+const bayern = 1;
+const rb_leipzip = 2;
+const koln = 3;
+const schalke = 4;
+const gladbach = 5;
+
+const win = 1;
+const draw = 0;
+const lose = -1;
+
+const games = [
+    [ bvb , bayern , win ],
+    [ bvb , rb_leipzip , lose ],
+    [ bvb , koln , win ],
+    [ bvb , schalke , lose ],
+    [ bvb , gladbach , win ],
+]
+
+// bvb
+// 
+
+/// create random weights for all inputs
+const weights = [
+    []
+]
+/// 
+
+const weights = [  ];
+
+
